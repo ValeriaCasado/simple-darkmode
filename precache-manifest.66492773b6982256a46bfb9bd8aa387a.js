e185d8e41366d90205577d5faa04697d91c4d4d3
Valeria Casado <ValeriaCasado@users.noreply.github.com>
2020-06-01 23:45:15 +0100

    Updates

---
+++ precache-manifest.66492773b6982256a46bfb9bd8aa387a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4aef5d96225720b918158fa1a14a76d",
+    "url": "/simple-darkmode/index.html"
+  },
+  {
+    "revision": "e44ce6a566ee74b2df49",
+    "url": "/simple-darkmode/static/css/main.21e2bfd2.chunk.css"
+  },
+  {
+    "revision": "788cd925a32fd7164392",
+    "url": "/simple-darkmode/static/js/2.67bb02e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simple-darkmode/static/js/2.67bb02e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44ce6a566ee74b2df49",
+    "url": "/simple-darkmode/static/js/main.456652c0.chunk.js"
+  },
+  {
+    "revision": "7935ea9dcf9979fd6af9",
+    "url": "/simple-darkmode/static/js/runtime-main.9c8492c8.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/simple-darkmode/static/media/logo.ee7cd8ed.svg"
+  }
+]);
